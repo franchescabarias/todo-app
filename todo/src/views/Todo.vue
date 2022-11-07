a491a2b138fa975902b867d741ffc6f98644e4e7
franchesca barias <franchesca.barias7@gmail.com>
2022-11-07 14:15:23 +0000

    tasks properties, doneTask method and colour when task is done added

--- todo/src/views/Todo.vue
+++ todo/src/views/Todo.vue
@@ -1,32 +1,67 @@
 <template>
-  <div class="home pa-6">
-    <v-list>
+  <div class="home">
+    <v-list class="pt-0">
 
       <v-list-item-group
         v-model="settings"
         multiple
       >
-        <v-list-item>
-          <template v-slot:default="{ active, }">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{'teal lighten-5' : task.done }"
+        >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
-                :input-value="active"
+                :input-value="task.done"
                 color="primary"
               ></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
             </v-list-item-content>
           </template>
         </v-list-item>
+        <v-divider></v-divider>
+      </div>
+
       </v-list-item-group>
     </v-list>
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'HomeView',
+    data() {
+      return {
+        tasks: [
+          {
+            id: '1',
+            title: 'First task',
+            done: false
+          },
+          {
+            id: '2',
+            title: 'Second task',
+            done: false
+          },
+          {
+            id: '3',
+            title: 'Third task',
+            done: false
+          }
+        ]
+      }
+      }, 
+      methods: {
+        doneTask(id) {
+          let task = this.tasks.filter(task => task.id === id)[0]
+          task.done = !task.done
+        }
+      }
     }
 </script>
