bd4c9877e4850ff2d37ce7a014f5b910a7a13c13
franchesca barias <franchesca.barias7@gmail.com>
2022-11-07 12:48:45 +0000

    nnavigation toolbar and drawer created with router view on home and about page

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,27 +1,80 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <!--  -->
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6 dark--text">
+              TODOApp
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title" :to='item.to'
+          link
+        >
+          <v-list-item-icon>
+            <v-icon class="dark">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title class="dark">{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app color="#fcb69f" dark clipped-left src="birds.jpg "
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <span><v-toolbar-title class="font-weight-bold">TODO</v-toolbar-title></span>
-      <span><v-toolbar-title>App</v-toolbar-title></span>
+      <v-app-bar-title class="font-weight-bold">TODO</v-app-bar-title>
+      <v-app-bar-title>App</v-app-bar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({ 
+      drawer: null, 
+      items: [
+          { title: 'My Tasks', icon: 'mdi-format-list-checks', to: '/'},
+          { title: 'About', icon: 'mdi-help-box', to: '/about'},
+        ],
+    }),
   }
 </script>
