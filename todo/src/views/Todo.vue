d665fba558abb3a8ca048178af9778105a352b7b
franchesca barias <franchesca.barias7@gmail.com>
2022-11-07 12:42:29 +0000

    first list created with checkbox

---
+++ todo/src/views/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home pa-6">
+    <v-list>
+
+      <v-list-item-group
+        v-model="settings"
+        multiple
+      >
+        <v-list-item>
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>Notifications</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeView',
+    }
+</script>
