46d6a2b6bb393d3f9d1c14134db2cf6b76349ada
franchesca barias <franchesca.barias7@gmail.com>
2022-11-07 09:48:03 +0000

    toolbar and title created

---
+++ todo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <span><v-toolbar-title class="font-weight-bold">TODO</v-toolbar-title></span>
+      <span><v-toolbar-title>App</v-toolbar-title></span>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
