241a3b9931a6012b5010748bceea415c984f35fd
franchesca barias <franchesca.barias7@gmail.com>
2022-11-15 13:33:31 +0000

    FieldAddTask.vue file added with text-field HTML and script

---
+++ todo/src/components/Todo/FieldAddTask.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-text-field
+             v-model="newTaskTitle"
+             @click:append="addTask"
+             @keyup.enter="addTask"
+             class="pa-3 blue--text pt-5"
+             outlined
+             hide-details
+             clearable 
+             label="Enter task"
+             append-icon="mdi-plus-box"
+           ></v-text-field>
+ </template>
+ 
+ <script>
+ export default {
+   data() {
+     return {
+       newTaskTitle: ''
+     }
+   }, 
+   methods: {
+     addTask() {
+       this.$store.commit('addTask', this.newTaskTitle)
+       this.newTaskTitle = ''
+     },
+       
+   }
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
