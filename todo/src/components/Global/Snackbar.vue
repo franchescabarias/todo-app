91a2dbd9d798632f6389c0d1370e6ef0eac77b03
franchesca barias <franchesca.barias7@gmail.com>
2022-11-16 12:41:18 +0000

    snackbar component

---
+++ todo/src/components/Global/Snackbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-snackbar
+    v-model="$store.state.snackbar.show"
+    shaped
+  >
+    {{ text }}
+
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        color="red"
+        text
+        v-bind="attrs"
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      text: `You've entered a task.`,
+    }),
+}
+</script>
+
+<style>
+
+</style>
